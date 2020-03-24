15b50ef95d175c10fcdccdce60bedfd860e90260
Luan Pires Bresolla <luanpires34@gmail.com>
2020-03-24 18:16:42 -0300

    rocketshoes

---
+++ frontend/src/pages/Home/index.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { MdAddShoppingCart } from 'react-icons/md';
+import api from '../../services/api';
+import { formatPrice } from '../../util/format';
+
+import * as CartActions from '../../store/modules/cart/actions';
+
+import { ProductList } from './styles';
+
+export default function Home() {
+  const [products, setProducts] = useState([]);
+
+  const amount = useSelector(state =>
+    state.cart.reduce((sumAmount, product) => {
+      sumAmount[product.id] = product.amount;
+
+      return sumAmount;
+    }, {})
+  );
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    async function loadProducts() {
+      const response = await api.get(`products`);
+
+      const data = response.data.map(product => ({
+        ...product,
+        priceFormatted: formatPrice(product.price),
+      }));
+
+      setProducts(data);
+    }
+
+    loadProducts();
+  }, []);
+
+  function handleAddProduct(id) {
+    /*
+    Quando é feita a conexão com o redux, temos acesso a este propriedade,
+    que serve para disparar uma ação, que são responsáveis por dizer para o redux
+    que queremos fazer alguma alteração no nosso estado
+    */
+
+    dispatch(CartActions.addToCartRequest(id));
+  }
+
+  return (
+    <ProductList>
+      {products.map(product => (
+        <li key={product.id}>
+          <img src={product.image} alt={product.title} />
+
+          <strong>{product.title}</strong>
+          <span>{product.priceFormatted}</span>
+
+          <button type="button" onClick={() => handleAddProduct(product.id)}>
+            <div>
+              <MdAddShoppingCart size={16} color="#FFF" />{' '}
+              {amount[product.id] || 0}
+            </div>
+
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+      ))}
+    </ProductList>
+  );
+}
---
+++ frontend/src/store/modules/cart/reducer.js
@@ -0,0 +1,37 @@
+import produce from 'immer';
+
+export default function cart(state = [], action) {
+  /*
+  Todos os reducers conseguem ouvir todas as actions que são efetuadas na aplicação,
+  por conta disso criamos um case para ele executar somente quando for a action de
+  ADD_TO_CART
+  */
+  switch (action.type) {
+    case '@cart/ADD_SUCCESS':
+      return produce(state, draft => {
+        const { product } = action;
+
+        draft.push(product);
+      });
+    case '@cart/REMOVE':
+      return produce(state, draft => {
+        const productIndex = draft.findIndex(p => p.id === action.id);
+
+        if (productIndex >= 0) {
+          draft.splice(productIndex, 1);
+        }
+      });
+
+    case '@cart/UPDATE_AMOUNT_SUCCESS': {
+      return produce(state, draft => {
+        const productIndex = draft.findIndex(p => p.id === action.id);
+
+        if (productIndex >= 0) {
+          draft[productIndex].amount = Number(action.amount);
+        }
+      });
+    }
+    default:
+      return state;
+  }
+}
